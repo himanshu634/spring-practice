e3d298ff1d7edaefadf3eb3cf9f7ca4e43d0ddc1
Himashu Mendapara <himanshumendapra@gmail.com>
2022-05-28 08:28:17 +0530

    minor changes

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -22,7 +22,12 @@ public class StudentController {
     }
 
     @PostMapping
-    public void registerNewStudent(@RequestBody Student student){
+    public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,8 +22,25 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
-    public void addNewStudent(Student student){
-        System.out.println(student);
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email is taken");
+        }
+
+        studentRepository.save(student);
+    }
+    
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+
+        if(!exists){
+            throw new IllegalStateException("Student does not exits");
+        }else {
+            studentRepository.deleteById(studentId);
+        }
+
     }
 
 }
